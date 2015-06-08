7c70f472fbd43f653c9386fe005dc82f04295ff5
Mohammad Moaaz Ahmad <13bscsmahmad@seecs.edu.pk>
2015-06-09 03:31:29 +0500

    Prepare db for IM

--- dumpfile.sql
+++ dumpfile.sql
@@ -1,19 +1,42 @@
--- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: project
--- ------------------------------------------------------
--- Server version	5.6.23-log
+-- Host: 127.0.0.1
+-- Generation Time: Jun 09, 2015 at 12:28 AM
+-- Server version: 5.6.23-log
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `im`
+--
+
+CREATE TABLE IF NOT EXISTS `im` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `To_ID` int(11) NOT NULL,
+  `From_ID` int(11) NOT NULL,
+  `Message` varchar(1000) COLLATE utf8_unicode_ci NOT NULL,
+  `Photo` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `DateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `status_upload`
@@ -26,12 +49,11 @@ CREATE TABLE IF NOT EXISTS `status_upload` (
   `Photo` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
   `Upload_DateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Table structure for table `user`
 --
-
 
 CREATE TABLE IF NOT EXISTS `user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
@@ -40,4 +62,8 @@ CREATE TABLE IF NOT EXISTS `user` (
   `profile_pic` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'defaultProfilePicture.png',
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Username` (`Username`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
