33ea757f9cb181eb1ee678b6c28c85b3a9a9d8d6
Mohammad Moaaz Ahmad <13bscsmahmad@seecs.edu.pk>
2015-06-09 03:06:00 +0500

    Latest sql dumpfile to support image uploads and profile pictures

--- dumpfile.sql
+++ dumpfile.sql
@@ -19,37 +19,25 @@
 -- Table structure for table `status_upload`
 --
 
-CREATE TABLE `status_upload` (
+CREATE TABLE IF NOT EXISTS `status_upload` (
   `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `UserID` int(10) unsigned NOT NULL,
   `Text` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
   `Photo` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
   `Upload_DateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
 --
 -- Table structure for table `user`
 --
 
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Username` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
   `Password` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `profile_pic` varchar(30) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'defaultProfilePicture.png',
   PRIMARY KEY (`ID`),
   UNIQUE KEY `Username` (`Username`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2015-06-04  0:37:34
+);
