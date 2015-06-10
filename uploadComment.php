75283231c086c8b0dd71378cad509cb96ab4a610
Mohammad Moaaz Ahmad <13bscsmahmad@seecs.edu.pk>
2015-06-10 19:37:02 +0500

    store comments on statuses from feed

--- uploadComment.php
+++ uploadComment.php
@@ -1,7 +1,44 @@
+<?php include 'filter.php'; ?>
+<?php include 'functions.php'; ?>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: MMA
- * Date: 06/10/2015
- * Time: 5:50 PM
- */
+
+if (loggedIn()){
+
+    // receive comment
+
+    $comment = $_REQUEST["comment"]; // store comment
+    $comment = addslashes($comment); // add slashes
+
+    $status_id = $_REQUEST["statusid"]; // get statusid
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "moaaz@dell";
+    $dbname = "project";
+    // Create connection
+
+    $link = mysqli_connect($servername, $username, $password, $dbname) or die('Could not connect: ' . mysqli_connect_error());
+
+    $sqlInsert = "Insert into comments (status_id, commented_user, comment) VALUES (\"" . $status_id . "\", \"" . $_SESSION["username"]. "\",\"" . $comment. "\");";
+
+    if ($link->query($sqlInsert) === TRUE){
+
+
+        echo "Your comment has been uploaded. Refreshing in a moment...";
+        echo "<script>setTimeout(\"location.href = 'home.php';\",1500);</script>";
+
+    } else {
+        echo $link->error;
+    }
+
+    $link->close();
+    return;
+
+
+
+} else {
+    header("location:index.php");
+}
+
+?>
